942089e6e97235498bc898c0f5460ba0fc7aa1c0
Simon <simonmarkholland@gmail.com>
2020-01-11 18:47:02 +0000

    Refactored home

--- home/models.py
+++ home/models.py
@@ -1,33 +1,45 @@
 from django.db import models
 from modelcluster.fields import ParentalKey
 from wagtail.core.models import Page, Orderable
-from wagtail.admin.edit_handlers import InlinePanel, FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.blocks import TextBlock, StructBlock
+from wagtail.admin.edit_handlers import InlinePanel, FieldPanel, StreamFieldPanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.core.fields import StreamField
+from tools.models import Seo, CommonStreamBlock, Item
 
-from tools.models import Dreamer, Seo
 
-import datetime
-dom = datetime.datetime.now().strftime ("%d")
-
-class HomePage(Page, Dreamer, Seo):
+class HomePage(Page, Seo):
+    heading = models.CharField(max_length=250, null=True, blank=True)
+    text = RichTextField(null=True, blank=True)
+    buttonLabel = models.CharField(max_length=255, null=True, blank=True)
+    buttonUrl = models.URLField(null=True, blank=True)
+    body = StreamField(CommonStreamBlock(required=False), null=True, blank=True)
 
     def get_context(self, request):
         context = super(HomePage, self).get_context(request)
-        context['menuitems'] = request.site.root_page.get_descendants(inclusive=True).live().in_menu()
-        # context['menuitems'] = self.get_children().filter(live=True, show_in_menus=True)
-
+        context['menuitems'] = request.site.root_page.get_descendants(
+            inclusive=True).live().in_menu()
 
         return context
 
-    subpage_types = ['tools.Index', 'tools.GoogleMaps', 'tools.GoogleCalendar', 'home.HomePage']
+    parent_page_types = ['wagtailcore.page', 'home.HomePage']
+    subpage_types = ['tools.Index', 'tools.GoogleMaps',
+                     'tools.GoogleCalendar', 'home.HomePage',
+                     'contact.ContactPage', 'contact.ContactSinglePage']
 
-
-    content_panels = Page.content_panels + Dreamer.panels + [
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('heading'),
+            FieldPanel('text'),
+            FieldPanel('buttonLabel'),
+            FieldPanel('buttonUrl'),
+        ], "Jumbotron"),
         InlinePanel('carousel_items', label="Carousel images"),
+        StreamFieldPanel('body', "Main content..."),
     ]
 
     promote_panels = Page.promote_panels + Seo.panels
-
 
     class Meta:
         verbose_name = "Home Page"
@@ -35,10 +47,13 @@ class HomePage(Page, Dreamer, Seo):
 
 
 class Carousel(Orderable):
-    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='carousel_items')
+    page = ParentalKey(HomePage, on_delete=models.CASCADE,
+                       related_name='carousel_items')
     title = models.CharField(blank=True, max_length=250)
     caption = models.CharField(blank=True, max_length=250)
-    image = models.ForeignKey('wagtailimages.Image',  null=True, blank=True, on_delete=models.CASCADE, related_name='+')
+    image = models.ForeignKey('wagtailimages.Image',  null=True,
+                              blank=True, on_delete=models.CASCADE, related_name='+')
+
     panels = [
         FieldPanel('title'),
         FieldPanel('caption'),
