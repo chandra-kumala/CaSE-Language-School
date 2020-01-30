269430ad47476325fd374246e43baa62c7060f4a
Simon <simonmarkholland@gmail.com>
2020-01-30 12:11:32 +0000

    Move all models into main models file

--- home/models.py
+++ home/models.py
@@ -1,20 +1,188 @@
 from django.db import models
-from modelcluster.fields import ParentalKey
 from wagtail.core.models import Page, Orderable
-from wagtail.core.fields import RichTextField
-from wagtail.core.blocks import TextBlock, StructBlock
-from wagtail.admin.edit_handlers import InlinePanel, FieldPanel, StreamFieldPanel, MultiFieldPanel
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    MultiFieldPanel,
+    StreamFieldPanel
+)
+from wagtail.core.fields import (
+    RichTextField, 
+    StreamField
+)
+from wagtail.core.blocks import (
+    URLBlock, 
+    TextBlock, 
+    StructBlock, 
+    StreamBlock, 
+    CharBlock, 
+    RichTextBlock, 
+    BooleanBlock
+)
+from wagtail.contrib.forms.models import (
+    AbstractEmailForm,
+    AbstractFormField
+)
+from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.core.fields import StreamField
+from wagtail.snippets.models import register_snippet
 
-from contact.models import Seo, CommonStreamBlock
+# from contact.models import Seo, CommonStreamBlock
+
+class CommonStreamBlock(StreamBlock):
+    heading = CharBlock(classname="full title", blank=True)
+    paragraph = RichTextBlock(blank=True)
+    embed = EmbedBlock(blank=True)
+    image = ImageChooserBlock(blank=True)
+    testimonial = StructBlock([
+        ('test_name', TextBlock(blank=True)),
+        ('test_quote', TextBlock(blank=True)),
+        ('test_reversed', BooleanBlock(required=False, default=False)),
+        ('test_pic', ImageChooserBlock(blank=True)),
+    ])
+    buttonLink = StructBlock([
+        ('text', TextBlock(blank=True)),
+        ('link', URLBlock(label="external URL", blank=True)),
+    ])
+    jumbotron = StructBlock([
+        ('heading', CharBlock(classname="full title", blank=True)),
+        ('text', TextBlock(blank=True)),
+        ('buttonLabel', CharBlock(classname="full label", blank=True)),
+        ('buttonUrl', URLBlock(blank=True)),
+    ])
+            
+
+    class Meta:
+        icon = 'cogs'
+
+
+class Seo(models.Model):
+    ''' Add extra seo fields to pages such as icons. '''
+    google_ad_code = models.CharField(max_length=50, null=True, blank=True)
+    seo_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="Optional social media image 300x300px image < 300kb."
+    )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                ImageChooserPanel('seo_image'),
+                FieldPanel('google_ad_code'),
+            ],
+            heading="Additional SEO options ...",
+        )
+
+    ]
+
+    class Meta:
+        """Abstract Model."""
+
+        abstract = True
+
+@register_snippet
+class Google(models.Model):
+    site_tag = models.CharField(max_length=255)
+
+    panels = [
+        FieldPanel('site_tag'),
+    ]
+
+    def __str__(self):
+        return self.site_tag
+
+@register_snippet
+class Social(models.Model):
+    css = models.CharField("List CSS Classes (eg. text-primary py-0)",
+                           max_length=255, null=True, blank=True)  # eg. text-primary py-0 fa-2x
+    title = models.CharField("Desc on hover (eg. December Bulletin)", max_length=255,
+                             null=True, blank=True)  # eg. Latest School Bulletin
+    link = models.CharField(
+        "Link to resource (eg tel:+62-061-661-6765)", max_length=255, null=True, blank=True)
+    icon = models.CharField(
+        "FA Icon (eg. fas fa-newspaper fa-fw fa-2x)", max_length=255, null=True, blank=True)
+    text = models.CharField("Visible text (eg. Latest School Bulletin)",
+                            max_length=255, null=True, blank=True)  # eg. Decembers Bulletin
+
+    panels = [
+        FieldPanel('css'),
+        FieldPanel('link'),
+        FieldPanel('title'),
+        FieldPanel('icon'),
+        FieldPanel('text'),
+    ]
+
+    def __str__(self):
+        return self.text
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        'ContactPage', 
+        on_delete=models.CASCADE,
+        related_name='form_fields',
+    )
+
+
+class ContactPage(AbstractEmailForm, Seo):
+    my_stream = StreamField(CommonStreamBlock(), null=True, blank=True,)
+    thank_you_text = RichTextField(blank=True)
+    css_label = 'Add CSS (FontAwesome and Bootstrap classes) '
+
+    button_css = models.CharField(max_length=300, 
+                    default='btn-success', 
+                    null=True, blank=True, 
+                    verbose_name= 'Button CSS',
+                    help_text= 'Classes from FontAwesome and Bootstrap can be used')
+    button_text = models.CharField(max_length=300, 
+                    default='Submit', 
+                    null=True, blank=True,
+                    help_text= 'FontAwesome icons can be used')
+ 
+    template = 'home/contact_page.html'
+    def get_context(self, request):
+        context = super(ContactPage, self).get_context(request)
+        context['menuitems'] = request.site.root_page.get_descendants(
+            inclusive=True).live().in_menu()
+
+        return context
+
+    content_panels = AbstractEmailForm.content_panels + [
+        StreamFieldPanel('my_stream'),
+        InlinePanel('form_fields', label='Form Fields'),
+        MultiFieldPanel([
+            FieldPanel('button_css'),
+            FieldPanel('button_text'),
+        ], heading='Button Settings'),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname='col6'),
+                FieldPanel('to_address', classname='col6'),
+            ]),
+            FieldPanel('subject'),
+        ], heading='Email Settings'),
+    ]
+    
+    promote_panels = AbstractEmailForm.promote_panels + Seo.panels
+
+class OrphanContactPage(ContactPage):
+        template = 'home/orphan_contact_page.html'
+
 
 class HomePage(Page, Seo):
     heading = models.CharField(max_length=250, null=True, blank=True)
     text = RichTextField(null=True, blank=True)
     buttonLabel = models.CharField(max_length=255, null=True, blank=True)
     buttonUrl = models.URLField(null=True, blank=True)
-    body = StreamField(CommonStreamBlock(required=False), null=True, blank=True)
+    my_stream = StreamField(CommonStreamBlock(required=False), null=True, blank=True)
 
     def get_context(self, request):
         context = super(HomePage, self).get_context(request)
@@ -22,10 +190,6 @@ class HomePage(Page, Seo):
             inclusive=True).live().in_menu()
 
         return context
-
-    subpage_types = ['tools.Index', 'tools.GoogleMaps',
-                     'tools.GoogleCalendar', 'home.HomePage',
-                     'contact.ContactPage']
 
     content_panels = Page.content_panels + [
         MultiFieldPanel([
@@ -35,7 +199,7 @@ class HomePage(Page, Seo):
             FieldPanel('buttonUrl'),
         ], "Jumbotron"),
         InlinePanel('carousel_items', label="Carousel images"),
-        StreamFieldPanel('body', "Main content..."),
+        StreamFieldPanel('my_stream', "Main content..."),
     ]
 
     promote_panels = Page.promote_panels + Seo.panels
