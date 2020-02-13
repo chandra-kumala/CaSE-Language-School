cc5fd38b18324956976f46a284188e6034411181
Simon <simonmarkholland@gmail.com>
2020-02-13 15:00:49 +0000

    Consolidate all advert tags into advert_tags and add Facebook

--- home/templatetags/google.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from home.models import Google
-
-register = template.Library()
-
-# Google snippets
-@register.simple_tag
-def google_tag():
-    return Google.objects.first().site_tag
