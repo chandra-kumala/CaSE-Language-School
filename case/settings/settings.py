c1e945448c1d48d2a6dc8895aa4cf6b5c9ce7244
Simon <simonmarkholland@gmail.com>
2020-02-27 16:10:24 +0000

    Shorten lines

--- case/settings/settings.py
+++ case/settings/settings.py
@@ -5,4 +5,6 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'webapp-728957.pythonanywhere.com', 'www.casemedan.com']
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'webapp-728957.pythonanywhere.com',
+                 'www.casemedan.com']
